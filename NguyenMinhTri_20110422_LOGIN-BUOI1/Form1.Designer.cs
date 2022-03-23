646fdac42c784e663edcf6767ff11c2b1c88c4de
Nguyễn Minh Trí <75057067+sktmaneytri@users.noreply.github.com>
2022-03-23 18:29:41 +0700

    Add files via upload

---
+++ NguyenMinhTri_20110422_LOGIN-BUOI1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace NguyenMinhTri_20110422_LOGIN_BUOI1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbForgetPass = new System.Windows.Forms.LinkLabel();
+            this.lbSignUp = new System.Windows.Forms.LinkLabel();
+            this.humanResourceCheck = new System.Windows.Forms.CheckBox();
+            this.studentCheck = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbForgetPass);
+            this.panel1.Controls.Add(this.lbSignUp);
+            this.panel1.Controls.Add(this.humanResourceCheck);
+            this.panel1.Controls.Add(this.studentCheck);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.txtUserName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(22, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(582, 384);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbForgetPass
+            // 
+            this.lbForgetPass.AutoSize = true;
+            this.lbForgetPass.Location = new System.Drawing.Point(286, 292);
+            this.lbForgetPass.Name = "lbForgetPass";
+            this.lbForgetPass.Size = new System.Drawing.Size(114, 17);
+            this.lbForgetPass.TabIndex = 11;
+            this.lbForgetPass.TabStop = true;
+            this.lbForgetPass.Text = "Forget Password";
+            this.lbForgetPass.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lbForgetPass_LinkClicked);
+            // 
+            // lbSignUp
+            // 
+            this.lbSignUp.AutoSize = true;
+            this.lbSignUp.Location = new System.Drawing.Point(58, 292);
+            this.lbSignUp.Name = "lbSignUp";
+            this.lbSignUp.Size = new System.Drawing.Size(56, 17);
+            this.lbSignUp.TabIndex = 10;
+            this.lbSignUp.TabStop = true;
+            this.lbSignUp.Text = "Sign up";
+            this.lbSignUp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lbSignUp_LinkClicked);
+            // 
+            // humanResourceCheck
+            // 
+            this.humanResourceCheck.AutoSize = true;
+            this.humanResourceCheck.Location = new System.Drawing.Point(289, 188);
+            this.humanResourceCheck.Name = "humanResourceCheck";
+            this.humanResourceCheck.Size = new System.Drawing.Size(140, 21);
+            this.humanResourceCheck.TabIndex = 9;
+            this.humanResourceCheck.Text = "Human Resource";
+            this.humanResourceCheck.UseVisualStyleBackColor = true;
+            // 
+            // studentCheck
+            // 
+            this.studentCheck.AutoSize = true;
+            this.studentCheck.Location = new System.Drawing.Point(107, 188);
+            this.studentCheck.Name = "studentCheck";
+            this.studentCheck.Size = new System.Drawing.Size(79, 21);
+            this.studentCheck.TabIndex = 8;
+            this.studentCheck.Text = "Student";
+            this.studentCheck.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(260, 27);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(94, 17);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "AccountLogin";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Lime;
+            this.btnLogin.Location = new System.Drawing.Point(289, 242);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(165, 31);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Red;
+            this.btnCancel.Location = new System.Drawing.Point(61, 242);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(143, 31);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(211, 142);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(243, 22);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(211, 75);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(243, 22);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UserName:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(627, 429);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox studentCheck;
+        private System.Windows.Forms.CheckBox humanResourceCheck;
+        private System.Windows.Forms.LinkLabel lbForgetPass;
+        private System.Windows.Forms.LinkLabel lbSignUp;
+    }
+}
+
---
+++ NguyenMinhTri_20110422_LOGIN-BUOI1/UpdateDeleteStudentForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace NguyenMinhTri_20110422_LOGIN_BUOI1
+{
+    public partial class UpdateDeleteStudentForm : Form
+    {   
+        public UpdateDeleteStudentForm()
+        {
+            InitializeComponent();
+        }
+        STUDENT student = new STUDENT();
+        private void search_btn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(id_text.Text);
+            SqlCommand command = new SqlCommand("SELECT id, fname, lname, bdate, gender, phone, address, picture FROM std WHERE id = " + id);
+            DataTable table = student.getStudents(command);
+            if(table.Rows.Count > 0)
+            {
+                fname_text.Text = table.Rows[0]["fname"].ToString();
+                lname_text.Text = table.Rows[0]["lname"].ToString();
+                bdate_text.Value = (DateTime)table.Rows[0]["bdate"];
+                if(table.Rows[0]["gender"].ToString() == "Female")
+                {
+                    male_btn.Checked = true;
+                } else
+                {
+                    female_btn.Checked = true;
+                }
+
+                phone_text.Text = table.Rows[0]["phone"].ToString();
+                address_text.Text = table.Rows[0]["address"].ToString();
+
+                byte[] pic = (byte[])table.Rows[0]["picture"];
+                MemoryStream picture = new MemoryStream(pic);
+                pictureBox.Image = Image.FromStream(picture);
+
+
+            } else
+            {
+                MessageBox.Show("not found", "Find student", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        //remove student
+        private void remove_btn_Click(object sender, EventArgs e)
+        {
+            DialogResult dg = MessageBox.Show("You want delete this student ?", "Notification", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dg == DialogResult.Yes)
+            {
+                try
+                {
+                    if (student.DeleteStudent(Int32.Parse(id_text.Text)))
+                    {
+                        MessageBox.Show("Delete succesfully", "Detele student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error!", "Delete student", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        
+
+        //check information
+        public bool checkInfor()
+        {
+            if (id_text.Text == "")
+            {
+                MessageBox.Show("Input the id ", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                id_text.Focus();
+                return false;
+            }
+            if (fname_text.Text == "")
+            {
+                MessageBox.Show("Input the first name", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fname_text.Focus();
+                return false;
+            }
+            if (lname_text.Text == "")
+            {
+                MessageBox.Show("Input the last name", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lname_text.Focus();
+                return false;
+            }
+            if (phone_text.Text == "")
+            {
+                MessageBox.Show("Input the phone", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                phone_text.Focus();
+                return false;
+            }
+            if (address_text.Text == "")
+            {
+                MessageBox.Show("Input the address", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                address_text.Focus();
+                return false;
+            }
+            return true;
+        }
+        //update student
+        private void edit_btn_Click(object sender, EventArgs e)
+        {
+            int id = Int32.Parse(id_text.Text);
+            string fname = fname_text.Text;
+            string lname = lname_text.Text;
+            DateTime bdate = bdate_text.Value;
+            string phone = phone_text.Text;
+            string adrs = address_text.Text;
+            string gender = "Male";
+            if (female_btn.Checked)
+            {
+                gender = "Female";
+            }
+            MemoryStream PIC = new MemoryStream();
+            try
+            {
+                if (student.EditStudent(id, fname, lname, bdate, phone, adrs, gender, PIC))
+                {
+                    MessageBox.Show("Delete succesfully", "Detele student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Error!", "Delete student", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //MessageBox.Show("Edit / Update information succesfully", "Edit student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
